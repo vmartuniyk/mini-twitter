b585d3b1f70e851c2def9c190b109b84a6ac14b8
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2021-01-03 23:04:09 +0200

    Start tweety project

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,12 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea name="body"  class="w-full placeholder-gray-500 text-gray-900" placeholder="What`s up, bro?">
+        </textarea>
+        <hr class="my-4">
+        <footer class="flex justify-between">
+            <img class="rounded-full mr-2" src="https://i.pravatar.cc/40" alt="Avatar">
+            <button type="submit" class="bg-blue-500 rounded-lg py-2 px-2 text-white shadow">Tweet a roo!</button>
+
+        </footer>
+    </form>
+</div>
---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="" class="font-bold text-lg mb-4 block">Home</a></li>
+    <li><a href="" class="font-bold text-lg mb-4 block">Explore</a></li>
+    <li><a href="" class="font-bold text-lg mb-4 block">List</a></li>
+    <li><a href="" class="font-bold text-lg mb-4 block">More</a></li>
+    <li><a href="" class="font-bold text-lg mb-4 block">Profile</a></li>
+</ul>
