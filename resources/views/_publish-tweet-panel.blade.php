93cee05cc6d82badc69f0ae1d9ea42c96541e651
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2021-01-04 16:57:50 +0200

    Dynamic Timeline

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,12 +1,17 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
-        <textarea name="body"  class="w-full placeholder-gray-500 text-gray-900" placeholder="What`s up, bro?">
+    <form method="POST" action="/tweets">
+        @csrf
+        <textarea name="body"  class="w-full placeholder-gray-500 text-gray-900" >
         </textarea>
         <hr class="my-4">
         <footer class="flex justify-between">
-            <img class="rounded-full mr-2" src="https://i.pravatar.cc/40" alt="Avatar">
+            <img class="rounded-full mr-2" src="{{ auth()->user()->avatar }}" alt="Avatar">
             <button type="submit" class="bg-blue-500 rounded-lg py-2 px-2 text-white shadow">Tweet a roo!</button>
 
         </footer>
     </form>
+
+    @error('body')
+        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
 </div>
--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,11 +1,11 @@
 <div class="flex p-4 border-b border-b-grey-400 ">
     <div class="mr-2 flex-shrink-0">
-        <img class="rounded-full mr-2" src="https://i.pravatar.cc/50" alt="Avatar">
+        <img class="rounded-full mr-2" src="{{ $tweet->user->avatar }}" alt="Avatar">
     </div>
     <div>
-        <h5 class="text-bold my-4">John Doe</h5>
+        <h5 class="text-bold my-4">{{ $tweet->user->name}}</h5>
         <p class="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, illum fugiat illo aliquid nulla minus ullam nesciunt deserunt, quisquam doloremque adipisci nisi soluta praesentium totam eaque accusamus qui quae tempore!
+            {{ $tweet->body }}
         </p>
     </div>
 </div>
